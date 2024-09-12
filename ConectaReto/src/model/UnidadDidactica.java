80cbce03dcdee567337dffd5f1d8dfee08d4cd98
2dam <2dam@TartangaLH.eus>
2024-09-12 13:19:43 +0200

    clase unidad didactica creada

--- ConectaReto/src/model/UnidadDidactica.java
+++ ConectaReto/src/model/UnidadDidactica.java
@@ -10,5 +10,90 @@ package model;
  * @author 2dam
  */
 public class UnidadDidactica {
+    private int id;
+    private String acronimo;
+    private String titulo;
+    private String evaluacion;
+    private String descripcion;
+
+    public UnidadDidactica(int id, String acronimo, String titulo, String evaluacion, String descripcion) {
+        this.id = id;
+        this.acronimo = acronimo;
+        this.titulo = titulo;
+        this.evaluacion = evaluacion;
+        this.descripcion = descripcion;
+    }
+    
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the acronimo
+     */
+    public String getAcronimo() {
+        return acronimo;
+    }
+
+    /**
+     * @param acronimo the acronimo to set
+     */
+    public void setAcronimo(String acronimo) {
+        this.acronimo = acronimo;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the evaluacion
+     */
+    public String getEvaluacion() {
+        return evaluacion;
+    }
+
+    /**
+     * @param evaluacion the evaluacion to set
+     */
+    public void setEvaluacion(String evaluacion) {
+        this.evaluacion = evaluacion;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
     
 }
