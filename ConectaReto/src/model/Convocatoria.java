a044016931574b48f3f4cbd128c468ab69529647
2dam <2dam@TartangaLH.eus>
2024-09-17 08:21:35 +0200

    Clases del model terminadas

--- ConectaReto/src/model/Convocatoria.java
+++ ConectaReto/src/model/Convocatoria.java
@@ -5,10 +5,56 @@
  */
 package model;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author 2dam
  */
 public class Convocatoria {
+    private String convocatoria;
+    private String descripcion;
+    private LocalDate fecha;
+    private String curso;
+
+    public Convocatoria(String convocatoria, String descripcion, LocalDate fecha, String curso) {
+        this.convocatoria = convocatoria;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+        this.curso = curso;
+    }
+
+    public String getConvocatoria() {
+        return convocatoria;
+    }
+
+    public void setConvocatoria(String convocatoria) {
+        this.convocatoria = convocatoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    
     
 }
--- ConectaReto/src/model/Enunciado.java
+++ ConectaReto/src/model/Enunciado.java
@@ -10,5 +10,58 @@ package model;
  * @author 2dam
  */
 public class Enunciado {
+    private int id;
+    private String descripcion;
+    private Dificultad nivel;
+    private boolean disponible;
+    private String ruta;
+
+    public Enunciado(int id, String descripcion, Dificultad nivel, boolean disponible, String ruta) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.nivel = nivel;
+        this.disponible = disponible;
+        this.ruta = ruta;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Dificultad getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(Dificultad nivel) {
+        this.nivel = nivel;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
     
 }
