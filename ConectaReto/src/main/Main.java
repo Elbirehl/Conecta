a18a7cc02fa68c0bff2732e9ff6af0d22279074b
2dam <2dam@TartangaLH.eus>
2024-09-17 08:50:46 +0200

    Conexión con base datos implementada y comprobada. Terminada la clase DBConnection

--- ConectaReto/src/main/Main.java
+++ ConectaReto/src/main/Main.java
@@ -5,6 +5,11 @@
  */
 package main;
 
+import controller.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author 2dam
@@ -16,6 +21,35 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        /*
+        // Instancia de DBConnection
+        DBConnection dbConnection = new DBConnection();
+        
+        // Inicializamos los objetos que vamos a utilizar
+        Connection con = null;
+        PreparedStatement stmt = null;
+
+        try {
+            // Intentamos abrir la conexión
+            con = dbConnection.openConnection();
+
+            // Comprobamos si la conexión fue exitosa
+            if (con != null) {
+                System.out.println("Conexión establecida correctamente.");
+            } else {
+                System.out.println("Error al establecer la conexión.");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                // Cerramos la conexión al terminar
+                dbConnection.closeConnection(stmt, con);
+            } catch (SQLException e) {
+                System.out.println("Error al intentar cerrar la conexión: " + e.getMessage());
+            }
+        }*/
     }
     
 }
